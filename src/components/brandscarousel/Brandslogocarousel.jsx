80ac5a84fde04ad5e2dc26a61507eb29e9cf9163
Aniket <aniketpratapsingh7310@gmail.com>
2026-01-25 23:58:42 +0530

    mobile res hompegae

--- src/components/brandscarousel/Brandslogocarousel.jsx
+++ src/components/brandscarousel/Brandslogocarousel.jsx
@@ -10,12 +10,11 @@ import Brandseven from "../assests/Brands/b_seven.avif";
 import Brandeight from "../assests/Brands/b_eight.avif";
 import Brandnine from "../assests/Brands/b_nine.avif";
 
-import Brandten from "../assests/Brands/b_ten.avif"
+import Brandten from "../assests/Brands/b_ten.avif";
 import Brandeleven from "../assests/Brands/b_eleven.avif";
 
-
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css"; 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const logos = [
@@ -30,37 +29,56 @@ const logos = [
   Brandeight,
   Brandnine,
   Brandten,
-  Brandeleven
+  Brandeleven,
 
   // add more logos as needed
 ];
 
 const Brandslogocarousel = () => {
+  const settings = {
+    infinite: true,
+    speed: 2000, // Adjust the speed for smooth scrolling
+    slidesToShow: 5, // Number of logos visible at a time
+    slidesToScroll: 1,
+    autoplay: true, // Enable autoplay
+    autoplaySpeed: 0, // Continuous autoplay speed
+    cssEase: "linear", // Smooth linear transition
+    variableWidth: true, // Allow each logo to adjust its width
+    arrows: false, // Hide navigation arrows
+    pauseOnHover: true, // Keeps autoplaying even when hovered
 
+      responsive: [
 
-    const settings = {
-        infinite: true,
-        speed: 2000,               // Adjust the speed for smooth scrolling
-        slidesToShow: 5,           // Number of logos visible at a time
-        slidesToScroll: 1,
-        autoplay: true,            // Enable autoplay
-        autoplaySpeed: 0,          // Continuous autoplay speed
-        cssEase: 'linear',         // Smooth linear transition
-        variableWidth: true,       // Allow each logo to adjust its width
-        arrows: false,             // Hide navigation arrows
-        pauseOnHover: true,       // Keeps autoplaying even when hovered
-      };
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+  };
   return (
     <div className="logo-slider">
       <Slider {...settings}>
         {logos.map((logo, index) => (
           <div key={index} className="slider-item">
-            <img src={logo} alt={`Logo ${index}`} style={{ width: '150px'}} />
+            <img src={logo} alt={`Logo ${index}`} style={{ width: "150px" }} />
           </div>
         ))}
       </Slider>
     </div>
-
   );
 };
 
